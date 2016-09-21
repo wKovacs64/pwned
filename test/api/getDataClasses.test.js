107d33238a11bbf8ca6cbfcce12dd2b3cd640603
Justin Hall <justin.r.hall@gmail.com>
2016-09-21 16:10:24 -0600

    Optimize getDataClasses tests

--- test/api/getDataClasses.test.js
+++ test/api/getDataClasses.test.js
@@ -1,7 +1,9 @@
 import expect from 'expect.js';
 import mockery from 'mockery';
 import sinon from 'sinon';
-import {data, loggerMock} from '../setup';
+import logger from '../../src/utils/logger';
+import spinner from '../../src/utils/spinner';
+import {data} from '../setup';
 
 describe('api: getDataClasses', () => {
   const hibpMockFound = {
@@ -23,147 +25,193 @@ describe('api: getDataClasses', () => {
   };
 
   let getDataClasses;
-  let spinner;
-  let spinnerStart;
-  let spinnerStop;
 
-  beforeEach(() => {
-    sinon.spy(loggerMock, 'log');
-    sinon.spy(loggerMock, 'error');
-    spinner = require('../../src/utils/spinner');
-    spinnerStart = sinon.stub(spinner, 'start');
-    spinnerStop = sinon.stub(spinner, 'stop');
-
+  before(() => {
     mockery.enable({
       useCleanCache: true,
       warnOnUnregistered: false
     });
-    mockery.registerMock('../utils/logger', loggerMock);
+    mockery.registerMock('../utils/logger', logger);
     mockery.registerMock('../utils/spinner', spinner);
   });
 
-  afterEach(() => {
-    loggerMock.log.restore();
-    loggerMock.error.restore();
-    spinnerStart.restore();
-    spinnerStop.restore();
-
+  after(() => {
     mockery.deregisterAll();
     mockery.disable();
   });
 
-  // ////////////////// spinner.start() ////////////////// //
-
-  it('should call spinner.start (!raw)', (done) => {
-    mockery.registerMock('hibp', hibpMockFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    getDataClasses(false);
-    expect(spinnerStart.called).to.be(true);
-    done();
+  beforeEach(() => {
+    sinon.stub(logger, 'log');
+    sinon.stub(logger, 'error');
+    sinon.stub(spinner, 'start');
+    sinon.stub(spinner, 'stop');
   });
 
-  it('should not call spinner.start (raw)', (done) => {
-    mockery.registerMock('hibp', hibpMockFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    getDataClasses(true);
-    expect(spinnerStart.called).to.be(false);
-    done();
+  afterEach(() => {
+    logger.log.restore();
+    logger.error.restore();
+    spinner.start.restore();
+    spinner.stop.restore();
   });
 
-  // ////////////////// spinner.stop() ////////////////// //
+  describe('found', () => {
+    before(() => {
+      mockery.resetCache();
+      mockery.registerMock('hibp', hibpMockFound);
+      getDataClasses = require('../../src/api/getDataClasses');
+    });
 
-  it('should call spinner.stop (non-error results, !raw)', () => {
-    expect(spinnerStop.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(false)
-        .then(() => {
-          expect(spinnerStop.called).to.be(true);
-        });
+    after(() => {
+      mockery.deregisterMock('hibp');
+    });
+
+    it('should call spinner.start (found && !raw)', (done) => {
+      getDataClasses(false);
+      expect(spinner.start.called).to.be(true);
+      done();
+    });
+
+    it('should not call spinner.start (found && raw)', (done) => {
+      getDataClasses(true);
+      expect(spinner.start.called).to.be(false);
+      done();
+    });
+
+    it('should call spinner.stop (found && !raw)', () => {
+      expect(spinner.stop.called).to.be(false);
+      return getDataClasses(false)
+          .then(() => {
+            expect(spinner.stop.called).to.be(true);
+          });
+    });
+
+    it('should not call spinner.stop (found && raw)', () => {
+      expect(spinner.stop.called).to.be(false);
+      return getDataClasses(true)
+          .then(() => {
+            expect(spinner.stop.called).to.be(false);
+          });
+    });
+
+    it('should call logger.log (found && !raw)', () => {
+      expect(logger.log.called).to.be(false);
+      return getDataClasses(false)
+          .then(() => {
+            expect(logger.log.callCount).to.be(1);
+          });
+    });
+
+    it('should call logger.log (found && raw)', () => {
+      expect(logger.log.called).to.be(false);
+      return getDataClasses(true)
+          .then(() => {
+            expect(logger.log.callCount).to.be(1);
+          });
+    });
   });
 
-  it('should not call spinner.stop (non-error results, raw)', () => {
-    expect(spinnerStop.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(true)
-        .then(() => {
-          expect(spinnerStop.called).to.be(false);
-        });
+  describe('not found', () => {
+    before(() => {
+      mockery.resetCache();
+      mockery.registerMock('hibp', hibpMockNotFound);
+      getDataClasses = require('../../src/api/getDataClasses');
+    });
+
+    after(() => {
+      mockery.deregisterMock('hibp');
+    });
+
+    it('should call spinner.start (notFound && !raw)', (done) => {
+      getDataClasses(false);
+      expect(spinner.start.called).to.be(true);
+      done();
+    });
+
+    it('should not call spinner.start (notFound && raw)', (done) => {
+      getDataClasses(true);
+      expect(spinner.start.called).to.be(false);
+      done();
+    });
+
+    it('should call spinner.stop (notFound && !raw)', () => {
+      expect(spinner.stop.called).to.be(false);
+      return getDataClasses(false)
+          .then(() => {
+            expect(spinner.stop.called).to.be(true);
+          });
+    });
+
+    it('should not call spinner.stop (notFound && raw)', () => {
+      expect(spinner.stop.called).to.be(false);
+      return getDataClasses(true)
+          .then(() => {
+            expect(spinner.stop.called).to.be(false);
+          });
+    });
+
+    it('should call logger.log (notFound && !raw)', () => {
+      expect(logger.log.called).to.be(false);
+      return getDataClasses(false)
+          .then(() => {
+            expect(logger.log.callCount).to.be(1);
+          });
+    });
+
+    it('should not call logger.log (notFound && raw)', () => {
+      expect(logger.log.called).to.be(false);
+      return getDataClasses(true)
+          .then(() => {
+            expect(logger.log.called).to.be(false);
+          });
+    });
   });
 
-  // ////////////////// breachData ////////////////// //
+  describe('error', () => {
+    before(() => {
+      mockery.resetCache();
+      mockery.registerMock('hibp', hibpMockError);
+      getDataClasses = require('../../src/api/getDataClasses');
+    });
 
-  it('should call logger.log (found && !raw)', () => {
-    expect(loggerMock.log.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(false)
-        .then(() => {
-          expect(loggerMock.log.callCount).to.be(1);
-        });
-  });
+    after(() => {
+      mockery.deregisterMock('hibp');
+    });
 
-  it('should call logger.log (found && raw)', () => {
-    expect(loggerMock.log.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(true)
-        .then(() => {
-          expect(loggerMock.log.callCount).to.be(1);
-        });
-  });
+    it('should call spinner.start (error && !raw)', (done) => {
+      getDataClasses(false);
+      expect(spinner.start.called).to.be(true);
+      done();
+    });
 
-  it('should call logger.log (notFound && !raw)', () => {
-    expect(loggerMock.log.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockNotFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(false)
-        .then(() => {
-          expect(loggerMock.log.callCount).to.be(1);
-        });
-  });
+    it('should not call spinner.start (error && raw)', (done) => {
+      getDataClasses(true);
+      expect(spinner.start.called).to.be(false);
+      done();
+    });
 
-  it('should not call logger.log (notFound && raw)', () => {
-    expect(loggerMock.log.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockNotFound);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(true)
-        .then(() => {
-          expect(loggerMock.log.called).to.be(false);
-        });
-  });
+    it('should call spinner.stop (error && !raw)', () => {
+      expect(spinner.stop.called).to.be(false);
+      return getDataClasses(false)
+          .then(() => {
+            expect(spinner.stop.called).to.be(true);
+          });
+    });
 
-  // ////////////////// error ////////////////// //
+    it('should not call spinner.stop (error && raw)', () => {
+      expect(spinner.stop.called).to.be(false);
+      return getDataClasses(true)
+          .then(() => {
+            expect(spinner.stop.called).to.be(false);
+          });
+    });
 
-  it('should call spinner.stop (error && !raw)', () => {
-    expect(spinnerStop.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockError);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(false)
-        .then(() => {
-          expect(spinnerStop.called).to.be(true);
-        });
-  });
-
-  it('should not call spinner.stop (error && raw)', () => {
-    expect(spinnerStop.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockError);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(true)
-        .then(() => {
-          expect(spinnerStop.called).to.be(false);
-        });
-  });
-
-  it('should call logger.error (error)', () => {
-    expect(loggerMock.error.called).to.be(false);
-    mockery.registerMock('hibp', hibpMockError);
-    getDataClasses = require('../../src/api/getDataClasses');
-    return getDataClasses(false)
-        .then(() => {
-          expect(loggerMock.log.called).to.be(false);
-          expect(loggerMock.error.called).to.be(true);
-        });
+    it('should call logger.error (error)', () => {
+      expect(logger.error.called).to.be(false);
+      return getDataClasses(false)
+          .then(() => {
+            expect(logger.log.called).to.be(false);
+            expect(logger.error.called).to.be(true);
+          });
+    });
   });
 });
