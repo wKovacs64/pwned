896039e432c7399331d4ec7af152150cd22e02f5
Justin Hall <justin.r.hall@gmail.com>
2016-09-21 17:26:51 -0600

    Split up spinner tests for consistency

--- test/utils/spinner.test.js
+++ test/utils/spinner.test.js
@@ -2,10 +2,18 @@ import expect from 'expect.js';
 import spinner from '../../src/utils/spinner';
 
 describe('util: spinner', () => {
-  it('should return an object with start and stop functions', (done) => {
+  it('should return an object', (done) => {
     expect(spinner).to.be.an('object');
+    done();
+  });
+
+  it('should have a "start" function', (done) => {
     expect(spinner).to.have.property('start');
     expect(spinner.start).to.be.a('function');
+    done();
+  });
+
+  it('should have a "stop" function', (done) => {
     expect(spinner).to.have.property('stop');
     expect(spinner.stop).to.be.a('function');
     done();
