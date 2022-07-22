f7c20fa7fffaac6bbe34134073c579d5940f5262
Justin Hall <justin.r.hall@gmail.com>
2022-07-22 12:03:39 -0600

    test: remove unnecessary disabling of lint rule

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,3 @@
-// TODO: figure out why eslint cannot resolve 'vitest/config'
-// eslint-disable-next-line import/no-unresolved
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
