20e6929a1f19f20d309c8ab48b666ddbe5ed19e2
Justin Hall <justin.r.hall@gmail.com>
2022-10-12 21:30:58 -0600

    chore: enable eslint caching

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['prettier --cache --write', 'eslint --fix'],
+  '*.{js,jsx,ts,tsx}': ['prettier --cache --write', 'eslint --cache --fix'],
   '*.{html,css,json,md,mdx,yml,yaml}': ['prettier --cache --write'],
 };
