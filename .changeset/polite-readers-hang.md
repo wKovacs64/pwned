a4882ae72ef03df46669198a546ee558ef8d2f1b
Justin Hall <justin.r.hall@gmail.com>
2024-07-17 13:19:26 -0600

    Always prefer apiKey interactive prompt

---
+++ .changeset/polite-readers-hang.md
@@ -0,0 +1,5 @@
+---
+'pwned': patch
+---
+
+Recommend the interactive prompt of the `apiKey` command over providing your key as a command-line argument (for enhanced security) when the API responds with 401 Unauthorized.
--- src/utils/translate-api-error.ts
+++ src/utils/translate-api-error.ts
@@ -4,8 +4,8 @@ export function translateApiError(originalMessage: string): string {
   const errMsg = originalMessage.includes('hibp-api-key')
     ? oneLine`
         Access denied due to invalid or missing API key. Please obtain an API
-        key from https://haveibeenpwned.com/API/Key and then run "pwned apiKey
-        <key>" to configure pwned.
+        key from https://haveibeenpwned.com/API/Key and then run "pwned apiKey"
+        to configure pwned.
       `
     : originalMessage;
 
