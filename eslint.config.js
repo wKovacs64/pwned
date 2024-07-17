2a0837edcf26b758d913ec3a0123a403534e3129
Justin Hall <justin.r.hall@gmail.com>
2024-07-17 12:58:58 -0600

    Replace eslint-plugin-wkovacs64 with @wkovacs64/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import baseConfig from '@wkovacs64/eslint-config';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+const config = [
+  ...baseConfig,
+  // overrides here
+];
+
+export default config;
